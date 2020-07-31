4ca6c928198a3a01d995d37c0077fe0154aa960e
Maximilian Faessler <maximilian@faesslerweb.de>
2020-07-31 13:44:57 +0000

    [EDIT] update minimum version rkw_registration

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
 			'typo3' => '7.6.0-8.7.99',
 			'rkw_mailer' => '8.7.0-8.7.99',
 			'rkw_basics' => '8.7.30-8.7.99',
-			'rkw_registration' => '8.7.0-8.7.99',
+			'rkw_registration' => '8.7.22-8.7.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
